f39c1d40d790b6d39667928c6c8e983405229c1c
teggot <fergon4ik@gmail.com>
2020-03-04 15:00:40 +0200

    Add tests processing

--- src/main.cr
+++ src/main.cr
@@ -40,6 +40,38 @@ class Lsp
     }
   end
 
+  private def parse_line(line_no : Int32, line : String)
+    result = /\s*def\s*(\w[\w\d]*)\s*\((\s*(?:\w[\w\d]*)?\s*(?:,\s*\w[\w\d]*)*\s*)\):/.match line
+    if result
+      arguments = Hash(String, Tuple(Int32, Int32)).new
+
+      name = result[1]
+      if raw_arguments = result[2]
+        matches = [] of Tuple(Int32, String)
+
+        post_match = raw_arguments
+        arg_start = result.begin(2).as Int32
+        while match = post_match.match(/(\w[\w\d]*)/)
+          matches << {arg_start + match.begin(1).as(Int32), match[1]}
+
+          arg_start = match.end(1).as(Int32) + arg_start
+          post_match = match.post_match
+        end
+
+        cur_match = 0
+
+        matches.each do |x|
+          arguments[x[1]] = {line_no, x[0]}
+          cur_match += 1
+        end
+      end
+
+      {name, line_no, result.begin(1).as Int32, arguments}
+    else
+      nil
+    end
+  end
+
   def implementation(request)
     cur_file_path = request["params"]["textDocument"]["uri"].as_s[7..]
     position = request["params"]["position"]
@@ -60,55 +92,35 @@ class Lsp
         line_no = -1
         file.each_line do |line|
           line_no += 1
+
           if line.includes? "@pytest.fixture"
             found_fixture = true
             next
           end
 
-          if found_fixture
-            if /^\s*@/.match line
-              next
-            elsif result = /\s*def\s*(\w[\w\d]*)\s*\((\s*(?:\w[\w\d]*)?\s*(?:,\s*\w[\w\d]*)*\s*)\):/.match line
-              if result
-                arguments = Hash(String, Tuple(Int32, Int32)).new
+          if line_info = parse_line line_no, line
+            name, line_no, col_no, arguments = line_info
 
-                name = result[1]
-                if raw_arguments = result[2]
-                  matches = [] of Tuple(Int32, String)
+            if found_fixture
+              fixture = Fixture.new file_path, name, line_no, col_no, arguments
+              fixtures << fixture
 
-                  post_match = raw_arguments
-                  arg_start = result.begin(2).as Int32
-                  while match = post_match.match(/(\w[\w\d]*)/)
-                      matches << { arg_start + match.begin(1).as(Int32), match[1] }
+              found_fixture = false
+            end
 
-                      arg_start = match.end(1).as(Int32) + arg_start
-                      post_match = match.post_match
-                  end
-
-                  cur_match = 0
-                  
-                  matches.each do |x|
-                    arguments[x[1]] = {line_no, x[0]}
-                    cur_match += 1
-                  end
-                end
-
-                fixture = Fixture.new file_path, name, line_no, result.begin(1).as Int32, arguments
-                fixtures << fixture
-
-                if file_path == cur_file_path && line_no == line_no_to_find 
-                    fixture.@arguments.each do |x|
-                        if x[1][1] <= col_number_to_find && col_number_to_find < x[1][1] + x[0].size
-                            searching_for_fixture = x[0]
-                            break
-                        end
-                    end
+            if file_path == cur_file_path && line_no == line_no_to_find
+              arguments.each do |x|
+                if x[1][1] <= col_number_to_find && col_number_to_find < x[1][1] + x[0].size
+                  searching_for_fixture = x[0]
+                  break
                 end
               end
-              found_fixture = false
+            end
+          elsif found_fixture
+            if /^\s*@/.match(line) || /^\s*$/.match(line)
+              next
             else
               found_fixture = false
-              next
             end
           end
         end
@@ -117,22 +129,21 @@ class Lsp
 
     if searching_for_fixture
       fixtures.each do |x|
-          if x.@name == searching_for_fixture
-              pos = {"line" => x.@line_number, "character" => x.@col_number}
-              implementation = {
-                  "uri" => "file://" + x.@file,
-                  "range" => {
-                      "start" => pos,
-                      "end" => pos
-                  }
-              }
-              break
-          end
+        if x.@name == searching_for_fixture
+          pos = {"line" => x.@line_number, "character" => x.@col_number}
+          implementation = {
+            "uri"   => "file://" + x.@file,
+            "range" => {
+              "start" => pos,
+              "end"   => pos,
+            },
+          }
+          break
+        end
       end
     end
 
     implementation
-    
   end
 
   def get_response(request)
